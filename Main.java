616698c9cb2dd716a56c7168085a8f81b0d9a4fc
David-Jimenez-Gon2604 <20253rd001@utez.edu.mx>
2026-02-18 13:13:43 -0600

    creamos nuestro tercer carro el civic del terrooooor

--- Main.java
+++ Main.java
@@ -13,15 +13,26 @@ public class Main{
         System.out.println(corvette.getCaja());
 
         Carros march = new Carros();
-        march.setMarca( "Stingray");
+        march.setMarca( "marcherraty");
         march.setSalida(2025);
-        march.setVelocidad("1,500 caballos de fuerza");
+        march.setVelocidad("90 caballos de fuerza");
         march.setCaja( true );
 
         System.out.println(march.getMarca());
         System.out.println(march.getSalida());
         System.out.println(march.getVelocidad());
         System.out.println(march.getCaja());
+
+        Carros civic = new Carros();
+        civic.setMarca( "civic del terror");
+        civic.setSalida(2001);
+        civic.setVelocidad("50,000 caballos de fuerza");
+        civic.setCaja( true );
+
+        System.out.println(civic.getMarca());
+        System.out.println(civic.getSalida());
+        System.out.println(civic.getVelocidad());
+        System.out.println(civic.getCaja());
 
     }
 }
